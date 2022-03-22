1dd803f1a332eb1a866a90a137378c8aec647813
EduardoOsiro <eduardoosiro1@gmail.com>
2022-03-22 17:34:21 -0300

    estilizacao feita

--- modulo2/props-labedin/labedin/src/components/CardPequeno/CardPequeno.js
+++ modulo2/props-labedin/labedin/src/components/CardPequeno/CardPequeno.js
@@ -1,15 +1,40 @@
 import React from "react";
-import './CardPequeno.css'
+import styled from 'styled-components'
+
+const CardPequenoContainer = styled.div`
+    display: flex;
+    align-items: center;
+    border: 1px solid black;
+    padding: 20px 10px;
+    margin-bottom: 10px;
+    height: 100px;
+    width: 40vw;
+`
+const CardPequenoImagem = styled.img`
+    width: 2vw;
+    margin-right: 10px;
+    border-radius: 50%;
+`
+const CardPequenoContainerDentro = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-items: flex-start;
+`
+
+const NomeCardPequeno = styled.h4`
+    margin-bottom: 15px;
+    font-family: bold;
+`
 
 function CardPequeno(props) {
     return (
-        <div className="smallcard-container">
-            <img src={ props.imagem } />
+        <CardPequenoContainer>
+            <CardPequenoImagem src={ props.imagem } />
             <div>
-                <h4>{ props.nome }</h4>
+                <NomeCardPequeno>{ props.nome }</NomeCardPequeno>
                 <p>{ props.descricao }</p>
             </div>
-        </div>
+        </CardPequenoContainer>
     )
 }
 
