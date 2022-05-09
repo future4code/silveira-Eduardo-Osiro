84db9c7b7603f901f5508594cbf8dcb2f255aa56
EduardoOsiro <eduardoosiro1@gmail.com>
2022-05-09 18:57:40 -0300

    parte do foumlário de login

--- modulo3/projeto-Labeddit/projeto-labbedit/src/Pages/LoginPage/LoginPage.js
+++ modulo3/projeto-Labeddit/projeto-labbedit/src/Pages/LoginPage/LoginPage.js
@@ -1,24 +1,40 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import { goToSignUpPage} from '../../Routes/Coordinator';
+import { goToSignUpPage } from '../../Routes/Coordinator';
+import Logo from '../../Assets/logo.png'
+import { LoginScreen, LoginForm } from './styledLoginPage'
+import useForm from '../../Hooks/useForm'
 
 
 
 function LoginPage() {
-  const navigate = useNavigate();
-  return (
-    <div>
-      <h2>Login</h2>
-      <input
-        placeholder='email'
-      />
-      <input
-        placeholder='password'
-      />
+  const {form, onChange, cleanFields} = useForm ({email: "", password: ""})
 
-      <button>Log In</button>
+  const navigate = useNavigate();
+
+  return (
+    <LoginScreen>
+      <img src={Logo} />
+      <h2>Login</h2>
+      <LoginForm>
+        <input
+          name="email"
+          value={form.email}
+          onChange={onChange}
+          placeholder='email'
+        />
+
+        <input
+          name="password"
+          value={form.password}
+          onChange={onChange}
+          placeholder='password'
+        />
+
+        <button>Log In</button>
+      </LoginForm>
       <button onClick={() => goToSignUpPage(navigate)}>Sign Up </button>
-    </div>
+    </LoginScreen>
   )
 }
 
---
+++ modulo3/projeto-Labeddit/projeto-labbedit/src/Pages/LoginPage/styledLoginPage.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const LoginScreen = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+
+    button {
+        width: 30%;
+    }
+
+    img {
+        max-width: 20%;
+    }
+`
+
+export const LoginForm = styled.form`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    button {
+        width: 64%;
+    }
+
+`
