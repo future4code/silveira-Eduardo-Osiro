df4dd394af282e097b8847c0c5dc2e4c61d0339d
EduardoOsiro <eduardoosiro1@gmail.com>
2022-05-10 21:24:27 -0300

    requisição de posts

--- modulo3/projeto-Labeddit/projeto-labbedit/src/Pages/FeedPage/FeedPage.js
+++ modulo3/projeto-Labeddit/projeto-labbedit/src/Pages/FeedPage/FeedPage.js
@@ -1,8 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Header from '../../Components/Header';
+import useForm from '../../Hooks/useForm';
+import useProtectedPage from '../../Hooks/useProtectedPage'
+import { post } from '../../Services/post';
+
 
 function FeedPage() {
+
+  useProtectedPage();
+
+  useEffect(() => {
+    post(setPosts)
+  }, [])
+
+  const [posts, setPosts] = useState
+
+  const { form, onChange, cleatFields } = useForm({ title: "", body: "" })
+
+  const postsList = posts && posts.map((post) => {
+    return (
+      <div>
+        <p>{post.title}</p>
+        <p>{post.username}</p>
+      </div>
+    )
+  });
+
+
   return (
-    <div>FeedPage</div>
+    <div>
+      <Header />
+      <div>
+        <h3>Feed</h3>
+
+        <input
+          name={"title"}
+          onChange={onChange}
+          placeholder="título"
+          required
+        />
+
+        <input
+          name={"body"}
+          onChange={onChange}
+          placeholder="Escreva seu post aqui"
+          required
+        />
+
+        <button>Postar!</button>
+
+      </div>
+      {postsList}
+    </div>
   )
 }
 
---
+++ modulo3/projeto-Labeddit/projeto-labbedit/src/Services/post.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+import { baseURL } from '../Constants/urls'
+
+const headers = {
+    headers: { authorization: localStorage.getItem("token") }
+}
+
+export const post = (setPosts) => {
+    axios.get(`${baseURL}/posts`, headers)
+        .then((response) => {
+            setPosts(response.data)
+        })
+        .catch((error) => {
+            alert(error.response.data)
+        })
+}
+
+export const createPost = (body) => {
+    axios.post(`${baseURL}/posts`, body, headers)
+    .then((response) => {
+        console.log(response.data.message)
+    })
+    .catch((error) => {
+        alert(error.response)
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
